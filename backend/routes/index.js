0dce98f76b8ceb786f59a34d5f4d8e527a5f28a2
Daniel Calderon <danielcalderon1199@gmail.com>
2023-08-01 15:10:51 -0700

    Setup router configuration and test

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,14 +1,8 @@
 // backend/routes/index.js
 const express = require('express');
+const apiRouter = require('./api');
 const router = express.Router();
 
-// Add a XSRF-TOKEN cookie
-router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
-  });
+router.use('/api', apiRouter);
 
 module.exports = router;
