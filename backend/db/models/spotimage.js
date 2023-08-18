5f50c78488595d89292bc3a78c0ff6a633d47eec
Daniel Calderon <danielcalderon1199@gmail.com>
2023-08-17 21:30:39 -0700

    Adding spotImages database and seeders

---
+++ backend/db/models/spotimage.js
@@ -0,0 +1,38 @@
+'use strict';
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class SpotImage extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      SpotImage.belongsTo(models.Spot, { foreignKey: 'spotId' });
+    }
+  }
+  SpotImage.init({
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    spotId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'SpotImages',
+        key: 'id'
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'SpotImage',
+  });
+  return SpotImage;
+};
